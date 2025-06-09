7bb96d972655bde645b3d6cb16fb7d876a6e61b7
StephAdongo <stephanie@kabarak.ac.ke>
2025-06-09 18:31:01 +0300

    Initial commit

--- shopping-cart2/vite.config.js
+++ shopping-cart2/vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test:{
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.js',
+    css: true,
+    coverage: {
+      provider: 'istanbul',
+      reporter: ['text', 'json', 'html'],
+      exclude: ['**/node_modules/**', '**/src/**/*.test.js', '**/src/**/*.test.jsx']
+    }
+  }
 })
