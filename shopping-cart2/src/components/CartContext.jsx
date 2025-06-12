562ba79376269e574255dffcb5279e6333e7154b
StephAdongo <stephanie@kabarak.ac.ke>
2025-06-12 11:36:35 +0300

    React context use on components

--- shopping-cart2/src/components/CartContext.jsx
+++ shopping-cart2/src/components/CartContext.jsx
@@ -1,3 +1,40 @@
 import React from 'react';
-import { createContext } from 'react';
-import defaulting from '/src/assets/shopping-bag.jpg';
+import {createContext} from 'react';
+import defaultImg from '/src/assets/shopping-bag.jpg';
+
+const defaultData =[
+ {
+    category: "category title",
+    id:1,
+    image:defaultImg,
+    imgURL:defaultImg,
+    price:10.00,
+    title:"Item Name",
+    description:"Item Description",
+    quantity:1
+},
+{
+    category: "category title",
+    id:2,
+    image:defaultImg,
+    imgURL:defaultImg,
+    price:20.00,
+    title:"Item Name",
+    description:"Item Description",
+    quantity:1
+},
+{
+    category: "category title",
+    id:3,
+    image:defaultImg,
+    imgURL:defaultImg,
+    price:30.00,
+    title:"Item Name",
+    description:"Item Description",
+    quantity:1
+}
+];
+
+const CartContext = createContext(defaultData);
+
+export default CartContext;
