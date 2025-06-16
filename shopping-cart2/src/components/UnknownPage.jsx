fda48e2adc3aa83413327ee03051e65ef2c9ce82
StephAdongo <stephanie@kabarak.ac.ke>
2025-06-16 13:37:22 +0300

    Added 404 error message page

--- shopping-cart2/src/components/UnknownPage.jsx
+++ shopping-cart2/src/components/UnknownPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {useNavigate} from 'react-router-dom';
 import {useState, useEffect} from 'react';
-import Styles from '/src/styles/unknownPage.module.css';
+import Styles from '/src/styles/UnknownPage.module.css';
 
 const UnknownPage = () => {
     const navigate = useNavigate();
